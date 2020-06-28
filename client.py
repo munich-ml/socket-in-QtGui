da9b6a120974026efe297248f9543bc2469731d5
Holger <53127691+munich-ml@users.noreply.github.com>
2020-06-28 19:51:41 +0200

    Implement client as class

--- client.py
+++ client.py
@@ -9,12 +9,44 @@ import socket
 from server import PORT_DEFAULT, HEADER_LENGTH, CODING
 
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((socket.gethostname(), PORT_DEFAULT))
+class HTesterTcpIpClient():
+    def __init__(self, port):
+        self.port = port
+        self.connected = False
+        
+    
+    def connect(self):
+        if not self.connected:
+            try:
+                self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.socket.connect((socket.gethostname(), self.port))
+                self.connected = True
 
-while True:
-    msg_length = int(client_socket.recv(HEADER_LENGTH).strip())
-    msg = client_socket.recv(msg_length)
-    print(msg.decode(CODING))
+            except Exception as e:
+                print("Exception during HTesterTcpIpClient.connect:\n", str(e))
+                self.connected = False
+        return self.connected
     
     
+    def disconnect(self):
+        if self.connected:
+            self.socket.close()
+            self.connected = False
+    
+
+    def receive(self):
+        if self.connected:
+            try:
+                msg_length = int(self.socket.recv(HEADER_LENGTH).strip())
+                msg = self.socket.recv(msg_length)
+                return msg.decode(CODING)
+            
+            except Exception as e:
+                print("Exception during HTesterTcpIpClient.receive:\n", str(e))
+                
+        return None
+            
+    
+    
+if __name__ == "__main__":
+    client = HTesterTcpIpClient(PORT_DEFAULT)
